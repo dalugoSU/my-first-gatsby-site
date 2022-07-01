36ec89171ef30ca0f8bb9a6a80982f56dd5ad816
dalugoSU <dalugo@syr.edu>
2022-07-01 02:11:35 -0600

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>I am Daniel Lugo and I am making this Pee Pee Poo Poo Website.</p>
+      <p>Powered by Gatsby :D</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
