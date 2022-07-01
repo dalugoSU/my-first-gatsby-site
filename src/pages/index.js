83f96b23c7ad0656bd475accba877317e8b49a9d
dalugoSU <dalugo@syr.edu>
2022-07-01 02:28:01 -0600

    Finished Gatsby Tutorial Part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Welcome to my Gatsby Site</p>
+      <StaticImage 
+        src='../images/me.jpeg' 
+        alt='A portrait of me dawg'>
+      </StaticImage>
     </Layout>
   )
 }
